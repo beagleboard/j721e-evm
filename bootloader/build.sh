b41cf7ef8e36c22a133cde179f3fa627d359fc6f
Robert Nelson <robertcnelson@gmail.com>
2021-07-21 14:04:09 -0500

    save build files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -9,6 +9,8 @@ ATF_TAG=master
 #https://github.com/OP-TEE/optee_os.git
 TEE_TAG=3.14.0
 
+time=$(date +%Y-%m-%d)
+
 if [ -d ./k3-image-gen/ ] ; then
 	rm -rf ./k3-image-gen/ || true
 fi
@@ -24,19 +26,40 @@ fi
 git clone -b ${KIG_TAG} https://github.com/rcn-ee/k3-image-gen --depth=1
 cd ./k3-image-gen/
 make CROSS_COMPILE=arm-linux-gnueabihf-
+
+if [ -f ./sysfw.itb ] ; then
+	mkdir -p ../deploy/${time}/
+	cp -v ./sysfw.itb ../deploy/${time}/
+else
+	echo "failure"
+	exit 2
+fi
+
 cd ../
 
 git clone -b ${ATF_TAG} https://github.com/ARM-software/arm-trusted-firmware.git --depth=1
 cd ./arm-trusted-firmware/
 make CROSS_COMPILE=aarch64-linux-gnu- ARCH=aarch64 PLAT=k3 TARGET_BOARD=generic SPD=opteed
-cd ../
 
-#; cp -v arm-trusted-firmware/build/k3/generic/release/bl31.bin ../debian/ ; cd ../
+if [ -f ./build/k3/generic/release/bl31.bin ] ; then
+	cp -v ./build/k3/generic/release/bl31.bin ../deploy/${time}/
+else
+	echo "failure"
+	exit 2
+fi
+
+cd ../
 
 git clone -b ${TEE_TAG} https://github.com/OP-TEE/optee_os.git --depth=1
 cd ./optee_os/
 make PLATFORM=k3-j721e CFG_ARM64_core=y
-cd ../
 
-#cp -v out/arm-plat-k3/core/tee-pager_v2.bin ../debian/ ; cd ../
+if [ -f ./out/arm-plat-k3/core/tee-pager_v2.bin ] ; then
+	cp -v ./out/arm-plat-k3/core/tee-pager_v2.bin ../deploy/${time}/
+else
+	echo "failure"
+	exit 2
+fi
+
+cd ../
 
